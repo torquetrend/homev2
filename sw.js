fc07255bf787630049f36b39efc1aed33bd89a03
torquetrend <visittorquetrend@gmail.com>
2024-10-23 20:03:03 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,38 @@
+const cacheName = 'torquetrend-cache-v1';
+const assetsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/scripts.js',
+    '/manifest.json',
+    'assets/images/car-charging.jpg',
+    'assets/images/lithium-mine.jpg',
+    'assets/images/waymo.jpg',
+    'assets/images/model-s.jpg',
+    'assets/images/M5.jpg',
+    'assets/images/eg80.jpg',
+    'assets/images/icons/icon-192x192.png',
+    'assets/images/icons/icon-512x512.png',
+    // Add other assets as needed
+];
 
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => {
+                return cache.addAll(assetsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
